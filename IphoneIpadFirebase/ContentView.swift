9c1f8310e9fd47c47a10f618bf69db2eaf672397
juancamilomejia <juamej@eltiempo.com>
2022-09-06 20:19:07 -0500

    Initial Commit

---
+++ IphoneIpadFirebase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IphoneIpadFirebase
+//
+//  Created by MacBook J&J  on 6/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IphoneIpadFirebase/IphoneIpadFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  IphoneIpadFirebaseApp.swift
+//  IphoneIpadFirebase
+//
+//  Created by MacBook J&J  on 6/09/22.
+//
+
+import SwiftUI
+
+@main
+struct IphoneIpadFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
